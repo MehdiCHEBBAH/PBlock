026c008d000b35d50b4c2b834cbbcd4fd23ff116
CHEBBAH Mehdi <gm_chebbah@esi.dz>
2019-05-01 15:15:01 +0100

    not working

--- beforeMain.js
+++ beforeMain.js
@@ -1,31 +1,3 @@
-chrome.webRequest.onHeadersReceived.addListener(function(details) {
-    for (i = 0; i < details.responseHeaders.length; i++) {
-   
-      if (isCSPHeader(details.responseHeaders[i].name.toUpperCase())) {
-        var csp = details.responseHeaders[i].value;
-   
-        // append "https://mysite.com" to the authorized sites
-        csp = csp.replace('script-src', 'script-src https://poveleceze.pythonanywhere.com/');
-        csp = csp.replace('style-src', 'style-src https://poveleceze.pythonanywhere.com/');
-   
-        details.responseHeaders[i].value = csp;
-      }
-    }
-   
-    return { // Return the new HTTP header
-      responseHeaders: details.responseHeaders
-    };
-  }, {
-    urls: ["https://*/*","http://*/*"],
-    types: ["main_frame"]
-  }, ["blocking", "responseHeaders"]);
-   
-   
-  function isCSPHeader(headerName) {
-    return (headerName == 'CONTENT-SECURITY-POLICY') || (headerName == 'X-WEBKIT-CSP');
-  }
-
-  
 // Content script is injecting a script with ES6 module support into the current page
 var s = document.createElement('script');
 s.src = chrome.runtime.getURL('main.js');
