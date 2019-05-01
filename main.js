7bb553da5c0100576e1edc647a6b0bc7c2236903
CHEBBAH Mehdi <gm_chebbah@esi.dz>
2019-05-01 14:31:41 +0100

    trying to solve CSP problem --> not working

--- main.js
+++ main.js
@@ -3,6 +3,34 @@ import {blockSite,errRequest} from './resultController.js'
 import {extractHost} from './extractController.js'
 
 
+chrome.webRequest.onHeadersReceived.addListener(function(details) {
+    for (i = 0; i < details.responseHeaders.length; i++) {
+   
+      if (isCSPHeader(details.responseHeaders[i].name.toUpperCase())) {
+        var csp = details.responseHeaders[i].value;
+   
+        // append "https://mysite.com" to the authorized sites
+        csp = csp.replace('script-src', 'script-src https://poveleceze.pythonanywhere.com/');
+        csp = csp.replace('style-src', 'style-src https://poveleceze.pythonanywhere.com/');
+   
+        details.responseHeaders[i].value = csp;
+      }
+    }
+   
+    return { // Return the new HTTP header
+      responseHeaders: details.responseHeaders
+    };
+  }, {
+    urls: ["https://*/*","http://*/*"],
+    types: ["main_frame"]
+  }, ["blocking", "responseHeaders"]);
+   
+   
+  function isCSPHeader(headerName) {
+    return (headerName == 'CONTENT-SECURITY-POLICY') || (headerName == 'X-WEBKIT-CSP');
+  }
+
+  
 /********************************************* site blocking **********************************************/
 var opts = {
 	q:extractHost()
